6765b444df3c0c81580140275d68887f27b9fda4
Georgy Gerbst <gerbst.g@gmail.com>
2019-11-26 23:37:43 +0300

    Task 2: switched to stream pipline

--- task1/task1.2/index.js
+++ task1/task1.2/index.js
@@ -1,27 +1,20 @@
 import fs from 'fs';
 import path from 'path';
 import csvToJson from 'csvtojson';
+import { pipeline } from 'stream';
 
 const csvFilePath = path.join(__dirname, '/example.csv');
 const fileOutputPath = path.join(__dirname, '/result.txt');
 
-csvToJson()
-	.fromFile(csvFilePath)
-	.subscribe((jsonObj) => {
-		return new Promise((resolve, reject) => {
-			resolve(jsonObj);
-		})
-	})
-	.then(jsonObj => {
-		jsonObj.map(obj => {
-			const objectString = `${JSON.stringify(obj)}\n`;
-			fs.appendFileSync(fileOutputPath, objectString, (writeErr) => {
-				if(writeErr) {
-					process.stdout.write(`Write error: ${writeErr}\n`);
-				}
-			});
-		})
-	})
-	.catch(err => {
-		process.stdout.write(`Subscription to file error: ${err}\n`);
-	})
+pipeline(
+	fs.createReadStream(csvFilePath),
+	csvToJson(),
+	fs.createWriteStream(fileOutputPath),
+	(err) => {
+		if (err) {
+			console.error('Pipeline failed.', err);
+		} else {
+			console.log('Pipeline succeeded.');
+		}
+	}
+);
