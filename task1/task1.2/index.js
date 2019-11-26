a49a6d99004a5e2a43538ea4cd79ed87a9618c21
Georgy Gerbst <gerbst.g@gmail.com>
2019-11-26 23:51:55 +0300

    switched to stream

--- task1/task1.2/index.js
+++ task1/task1.2/index.js
@@ -6,15 +6,40 @@ import { pipeline } from 'stream';
 const csvFilePath = path.join(__dirname, '/example.csv');
 const fileOutputPath = path.join(__dirname, '/result.txt');
 
+// fs.createReadStream(csvFilePath)
+// 	.pipe(csvToJson())
+// 	.pipe(fs.createWriteStream(fileOutputPath));
+
 pipeline(
 	fs.createReadStream(csvFilePath),
 	csvToJson(),
 	fs.createWriteStream(fileOutputPath),
 	(err) => {
 		if (err) {
-			console.error('Pipeline failed.', err);
+		console.error('Pipeline failed.', err);
 		} else {
-			console.log('Pipeline succeeded.');
+		console.log('Pipeline succeeded.');
 		}
 	}
-);
+	);
+
+// csvToJson()
+// 	.fromFile(csvFilePath)
+// 	.subscribe((jsonObj) => {
+// 		return new Promise((resolve, reject) => {
+// 			resolve(jsonObj);
+// 		})
+// 	})
+// 	.then(jsonObj => {
+// 		jsonObj.map(obj => {
+// 			const objectString = `${JSON.stringify(obj)}\n`;
+// 			fs.appendFileSync(fileOutputPath, objectString, (writeErr) => {
+// 				if(writeErr) {
+// 					process.stdout.write(`Write error: ${writeErr}\n`);
+// 				}
+// 			});
+// 		})
+// 	})
+// 	.catch(err => {
+// 		process.stdout.write(`Subscription to file error: ${err}\n`);
+// 	})
