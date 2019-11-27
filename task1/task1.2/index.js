cfaf00b85f51661e6025c72a71a4d62d59db27e0
Georgy Gerbst <gerbst.g@gmail.com>
2019-11-28 00:30:14 +0300

    Not finished. Reading file line by line

--- task1/task1.2/index.js
+++ task1/task1.2/index.js
@@ -1,45 +1,22 @@
 import fs from 'fs';
 import path from 'path';
+import readline from 'readline';
 import csvToJson from 'csvtojson';
-import { pipeline } from 'stream';
 
 const csvFilePath = path.join(__dirname, '/example.csv');
 const fileOutputPath = path.join(__dirname, '/result.txt');
 
-// fs.createReadStream(csvFilePath)
-// 	.pipe(csvToJson())
-// 	.pipe(fs.createWriteStream(fileOutputPath));
+const input = fs.createReadStream(csvFilePath);
+const output = fs.createWriteStream(fileOutputPath, {
+	flags: 'w',
+});
 
-pipeline(
-	fs.createReadStream(csvFilePath),
-	csvToJson(),
-	fs.createWriteStream(fileOutputPath),
-	(err) => {
-		if (err) {
-		console.error('Pipeline failed.', err);
-		} else {
-		console.log('Pipeline succeeded.');
-		}
-	}
-	);
+const inputFileLine = readline.createInterface({
+	input,
+	terminal: false,
+	output,
+})
 
-// csvToJson()
-// 	.fromFile(csvFilePath)
-// 	.subscribe((jsonObj) => {
-// 		return new Promise((resolve, reject) => {
-// 			resolve(jsonObj);
-// 		})
-// 	})
-// 	.then(jsonObj => {
-// 		jsonObj.map(obj => {
-// 			const objectString = `${JSON.stringify(obj)}\n`;
-// 			fs.appendFileSync(fileOutputPath, objectString, (writeErr) => {
-// 				if(writeErr) {
-// 					process.stdout.write(`Write error: ${writeErr}\n`);
-// 				}
-// 			});
-// 		})
-// 	})
-// 	.catch(err => {
-// 		process.stdout.write(`Subscription to file error: ${err}\n`);
-// 	})
+inputFileLine.on('line', (line) => {	
+	// ...
+})
